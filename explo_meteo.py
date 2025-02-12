57eddfc99d34e846dbd892cb2380669100a4c296
Leo Burgund <leo.burgund@gmail.com>
2025-02-12 19:52:27 +0100

    zz

--- explo_meteo.py
+++ explo_meteo.py
@@ -17,25 +17,6 @@ df = df.drop(columns=df.columns[df.isna().mean() > 0.5].tolist())
 # %%
 df.columns
 # %%
-# Drop les colonnes géo redondantes; on garde le nom_dept, nom_region
-# (redondant? mais utile pour le join?), longi/lati (utile pour les villes?)
-# Pas mal de noms epci mais au final c'est juste des doublons formattés différemment
-df = df.drop(
-  columns=[
-    # "numer_sta",
-    "coordonnees",
-    "nom",
-    "libgeo",
-    "codegeo",
-    "code_epci",
-    "code_dep",
-    "code_reg",
-    "nom_epci",
-  ]
-)
-# Drop colonnes inutiles
-df = df.drop(columns=["mois_de_l_annee", "t", "tminsol"])
-# %%
 # Détails de la localisation de chaque station
 # 1 station max par département
 # Au moins 1 station dans chaque région
@@ -46,7 +27,7 @@ geo_details
 # On remarque que toutes les villes ont une station proche, sauf Grenoble
 # On pourra donc pour la construction du modèle lier les données météo de chaque station
 # à la ville correspondante, sauf pour Grenoble pour laquelle on peut faire la moyenne
-# des trois stations
+# des trois stations les plus proches, plus ou moins équidistantes
 
 # On prends les longitudes/latitudes des stations
 df[["longitude", "latitude"]].round(9).value_counts()
@@ -63,9 +44,8 @@ tuple_villes = (villes_loc["Longitude"], villes_loc["Latitude"], villes_loc["Vil
 utils.plot_france_map(tuple_stations, tuple_villes)
 
 # %%
-# TODO: distance euclidienne pour chaque ville aux stations, trouver la station la
-# plus proche (département), pour Grenoble les 3 plus proches
-# Calculate the Euclidean distance between each city and each station
+# Distance euclidienne pour chaque ville aux stations, on trouve la station la
+# plus proche (1 par département), pour Grenoble les 3 plus proches
 distances = [
   (
     ville["Ville"],
@@ -91,6 +71,32 @@ for ville, station_info in closest_stations.items():
       print(ville, ":", station[3])
   else:
     print(ville, ":", station_info[3])
+
+# %%
+df.columns
+col = [
+  "type_de_tendance_barometrique",
+]
+# %%
+# Drop les colonnes géo redondantes; on garde le nom_dept, nom_region
+# Drop des autres colonnes inutiles, trop de NaN, infos pas vraiment pertinentes
+# ou redondantes (température, localisations, ...)
+df = df.drop(
+  columns=[
+    "numer_sta",
+    "coordonnees",
+    "nom",
+    "libgeo",
+    "codegeo",
+    "code_epci",
+    "code_dep",
+    "code_reg",
+    "nom_epci",
+    "mois_de_l_annee",
+    "t",
+    "tminsol",
+  ]
+)
 # %%
 # S'occuper de temps_present / passe et ww / w1 / w2
 # Vraiment pas bcp de w1, w2 donc tout drop sauf temps présent
@@ -98,36 +104,5 @@ col = ["ww", "w1", "w2", "temps_passe_1"]
 df.drop(columns=col, inplace=True)
 df.head()
 
-# %%
-lat_stations = df["latitude"].unique()
-longi_stations = df["longitude"].unique()
-lat_stations.shape, longi_stations.shape
-sorted(longi_stations)
-
-# %%
-df.loc[df["longitude"] == "1.396666999999999", "longitude"] = "1.396667"
-df.loc[df["longitude"] == "3.7640000000000002", "longitude"] = "3.764"
-lat_stations = df["latitude"].unique()
-longi_stations = df["longitude"].unique()
-print(lat_stations.shape, longi_stations.shape)
-print(sorted(longi_stations))
-
-# %%
-df.loc[df["longitude"] == "1.396667"]
-
-# %%
-print(sorted(longi_stations))
-
-# %%
-
-# Example longitude and latitude data
-longi_stations = [-73.935242, -118.243683, -0.127758]
-lat_stations = [40.730610, 34.052235, 51.507351]
-
-plt.figure(figsize=(8, 6))
-plt.scatter(longi_stations, lat_stations, color="red")
-plt.title("Scatter Plot of Longitude and Latitude")
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.grid(True)
-plt.show()
+# TODO Créer fonction tweak_meteo
+# TODO Voir la dernière cellule, check les colonnes de la df grader que les utiles
