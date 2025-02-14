0125105c7aa6b7b0f63df239601c1eb71713e8ce
Leo Burgund <leo.burgund@gmail.com>
2025-02-14 20:02:08 +0100

    model

--- simple_model.py
+++ simple_model.py
@@ -5,13 +5,16 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 from sklearn.metrics import root_mean_squared_error
-
-np.random.seed(42)
+from prophet import Prophet
 
 # %%
+np.random.seed(42)
 df = pd.read_parquet("data/geo_tweaked.parquet")
 
 # %%
+# TODO Transform variables into dummy with pandas
+# %%
+# RandomForest avec TimeseriesCrossVal
 X = df[["dayofweek", "dayofyear"]].values
 
 tscv = TimeSeriesSplit()
@@ -31,7 +34,29 @@ for zone in zone_list:
         err_test = root_mean_squared_error(y_test, y_pred)
         print(f"{zone}, fold {fold}: err_train={err_train:.3f} err_test={err_test:.3f}")
         fold += 1
-        if zone == "France":
+        if zone == "France" and fold == 5:
             plt.plot(y_test, linewidth=1, alpha=0.5)
             plt.plot(y_pred, linewidth=1, alpha=0.8)
             plt.show()
+
+# %%
+# RandomForest sur 2021
+
+# %%
+# Prophet sur 2021
+df2 = pd.DataFrame()
+df2["y"] = df["France"]
+df2["ds"] = df.index
+df2["ds"] = df2["ds"].dt.tz_localize(None)
+df_train = df2[df2.index < "2021"]
+df_test = df2[df2.index >= "2021"]
+df_train
+# %%
+m = Prophet()
+m.fit(df_train)
+# %%
+forecast = m.predict(df_test)
+fig = m.plot(forecast)
+# %%
+root_mean_squared_error(df_test["y"], forecast["yhat"])
+m.plot_components(forecast)
