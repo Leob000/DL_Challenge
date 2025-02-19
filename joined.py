1bede2ee382d60362197386a52013c1bcd0b0236
Leo Burgund <leo.burgund@gmail.com>
2025-02-19 10:16:10 +0100

    Feature eng on temp

--- joined.py
+++ joined.py
@@ -1,6 +1,9 @@
 # %%
 import pandas as pd
 import missingno as msno
+import matplotlib.pyplot as plt
+
+OPTION_FULL_ANALYSIS = False
 
 # %%
 df_geo = pd.read_parquet("data/geo_tweaked.parquet")
@@ -23,8 +26,6 @@ df_geo = df2.copy()
 df = pd.merge(df_geo, df_meteo, on=["date", "zone"])
 
 # %%
-msno.matrix(df.sort_values(by="date"))
-# %%
 # On doit drop l'heure de changement d'heure octobre (présent dans l'index mais NaN pour geo, valeurs présentes pour meteo)
 print(df.shape)
 df.loc[(df["Load"].isna()) & (df["date"] < "2022")]
@@ -45,7 +46,13 @@ problem_dates = [
 for dte in problem_dates:
     df.drop(df.loc[df["date"] == dte].index, inplace=True)
 print(df.shape)
-df.loc[(df["Load"].isna()) & (df["date"] < "2022")]
+# df.loc[(df["Load"].isna()) & (df["date"] < "2022")] # Visu
+
+# %%
+# On drop les rows qui n'ont pas de valeur de conso
+df_no_NaN = df[df["date"] < "2022"].dropna()
+df = pd.concat([df_no_NaN, df[df["date"] >= "2022"]])
+# msno.matrix(df) # Visu
 
 # %%
 for ville in villes:
@@ -60,5 +67,39 @@ df.loc[df["zone"] == "France", "is_pays"] = 1
 df["is_ville"] = df["is_ville"].fillna(0)
 df["is_reg"] = df["is_reg"].fillna(0)
 df["is_pays"] = df["is_pays"].fillna(0)
+
 # %%
-df
+# Feature eng, binning temp
+# On met la date en index
+df2 = df.copy()
+df2 = df2.set_index("date")
+df2["temp_below_15"] = df2["tc"] < 15
+
+zones = df2["zone"].unique().tolist()
+
+for zon in zones:
+    df2.loc[df2["zone"] == zon, "tc_ewm15"] = (
+        df2.loc[df2["zone"] == zon, "tc"].ewm(alpha=0.15).mean()
+    )
+    df2.loc[df2["zone"] == zon, "tc_ewm06"] = (
+        df2.loc[df2["zone"] == zon, "tc"].ewm(alpha=0.06).mean()
+    )
+    df2.loc[df2["zone"] == zon, "tc_ewm15_max24h"] = (
+        df2.loc[df2["zone"] == zon, "tc_ewm15"].rolling("24h").max()
+    )
+    df2.loc[df2["zone"] == zon, "tc_ewm15_min24h"] = (
+        df2.loc[df2["zone"] == zon, "tc_ewm15"].rolling("24h").min()
+    )
+
+if OPTION_FULL_ANALYSIS:
+    for i in ["tc", "tc_ewm15", "tc_ewm06"]:
+        plt.plot(
+            df2.loc[(df2["zone"] == "Paris") & (df2.index <= "2017-02-18"), i],
+            linewidth=1,
+            alpha=0.5,
+        )
+    plt.show()
+
+df = df2.copy()
+
+# %%
