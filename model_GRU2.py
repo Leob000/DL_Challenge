0185d17bde077736c93faeef53864ad8c973c2ad
Leo Burgund <leo.burgund@gmail.com>
2025-02-26 14:47:00 +0100

    Add feature selection and normalization options in model_GRU2.py

--- model_GRU2.py
+++ model_GRU2.py
@@ -5,6 +5,9 @@ from torch.utils.data import Dataset
 
 COLAB = False
 FULL_TRAIN = False
+DROP_AUGUSTS_FLAGS = True
+DROP_PRECIPITATIONS = True
+DROP_PRESSION = True
 # %%
 # Chargement dataset/GPU (Colab/MacOS/CPU)
 
@@ -22,6 +25,70 @@ else:
         device = torch.device("mps")  # MacOS GPU
 
 print("Device used:", device)
+# %%
+# Feature selection, on drop ou non certaines features
+features_to_normalize = [
+    "ff",
+    "tc",
+    "u",
+    "tc_ewm15",
+    "tc_ewm06",
+    "tc_ewm15_max24h",
+    "tc_ewm15_min24h",
+]
+
+df = df.drop(columns=["is_ville", "is_reg", "is_pays"])
+
+if DROP_AUGUSTS_FLAGS:
+    df = df.drop(columns=["is_august", "is_july_or_august"])
+
+if DROP_PRECIPITATIONS:
+    df = df.drop(columns=["rr1"])
+else:
+    features_to_normalize.append("rr1")
+
+if DROP_PRESSION:
+    df = df.drop(columns=["pres"])
+else:
+    features_to_normalize.append("pres")
+
+# %%
+# On normalise les variables continues
+# On garde en liste les moyennes et std des Loads des différentes régions pour renormaliser les pred à la fin
+li_zones = df["zone"].unique().tolist()
+
+# Standardization per zone de "Load"
+li_load = []
+for zone in li_zones:
+    load_temp = (
+        zone,
+        df.loc[df["zone"] == zone, "Load"].mean(),
+        df.loc[df["zone"] == zone, "Load"].std(),
+    )
+    li_load.append(load_temp)
+    df.loc[df["zone"] == zone, "Load"] = (
+        df.loc[df["zone"] == zone, "Load"] - df.loc[df["zone"] == zone, "Load"].mean()
+    ) / df.loc[df["zone"] == zone, "Load"].std()
+
+
+# Standardization of the rest, per zone or globally
+for zone in li_zones:
+    for feature in features_to_normalize:
+        df.loc[df["zone"] == zone, feature] = (
+            df.loc[df["zone"] == zone, feature]
+            - df.loc[df["zone"] == zone, feature].mean()
+        ) / df.loc[df["zone"] == zone, feature].std()
+
+
+# Fonction à utilier plus tard pour rescale
+# TODO A changer plus tard en fonction de quand et comment rescale les trucs
+def rescale(dff, col):
+    for zone, mean, std in li_load:
+        dff.loc[dff["zone"] == zone, col] = (
+            dff.loc[dff["zone"] == zone, col] * std + mean
+        )
+
+
 # %%
 # Séparation du dataset en train et test set
 if FULL_TRAIN:
